1e057ef8258c98f985130cd68bce4a3957927dfe
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-03-09 09:58:09 +0800

    initial commit

---
+++ app/src/main/java/com/ralphmarondev/compose_notification/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.ralphmarondev.compose_notification
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.ralphmarondev.compose_notification.ui.theme.ComposeNotificationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeNotificationTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
